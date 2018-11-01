2aabd06a25810b0acd5ac80362a0a5da7f3f27f6
dev <tomas.pospisil180695@gmail.com>
2018-11-01 21:31:46 +0100

    Přidejte soubory projektu.

---
+++ Přestřižená šňůra/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Přestřižená_šňůra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tlPřestřihni = new System.Windows.Forms.Button();
+            this.tlZnovu = new System.Windows.Forms.Button();
+            this.časovač = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tlPřestřihni
+            // 
+            this.tlPřestřihni.Location = new System.Drawing.Point(52, 218);
+            this.tlPřestřihni.Name = "tlPřestřihni";
+            this.tlPřestřihni.Size = new System.Drawing.Size(75, 23);
+            this.tlPřestřihni.TabIndex = 0;
+            this.tlPřestřihni.Text = "Přestřihni";
+            this.tlPřestřihni.UseVisualStyleBackColor = true;
+            this.tlPřestřihni.Click += new System.EventHandler(this.tlPřestřihni_Click);
+            // 
+            // tlZnovu
+            // 
+            this.tlZnovu.Location = new System.Drawing.Point(164, 218);
+            this.tlZnovu.Name = "tlZnovu";
+            this.tlZnovu.Size = new System.Drawing.Size(75, 23);
+            this.tlZnovu.TabIndex = 1;
+            this.tlZnovu.Text = "Znovu";
+            this.tlZnovu.UseVisualStyleBackColor = true;
+            this.tlZnovu.Click += new System.EventHandler(this.tlZnovu_Click);
+            // 
+            // časovač
+            // 
+            this.časovač.Enabled = true;
+            this.časovač.Interval = 50;
+            this.časovač.Tick += new System.EventHandler(this.časovač_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tlZnovu);
+            this.Controls.Add(this.tlPřestřihni);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tlPřestřihni;
+        private System.Windows.Forms.Button tlZnovu;
+        private System.Windows.Forms.Timer časovač;
+    }
+}
+
---
+++ Přestřižená šňůra/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Přestřižená_šňůra
+{
+    public partial class Form1 : Form
+    {
+
+        //parametry kružnice
+        int xStředuKružnice = 100;
+        int yStředuKružnice = 100;
+        int poloměrKružnice = 60;
+
+        //parametry kuličky
+
+        double úhel = 0; //Úhel průvodiče ve stupních
+        double omega = 80; //Úhlová rychlost ve stupních za sekundu
+        double xStředuKuličky, yStředuKuličky;
+        double vx, vy;
+        int poloměrKuličky = 5;
+
+        //Fáze programu příznak přestřižení
+        bool přestřiženo = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void časovač_Tick(object sender, EventArgs e)
+        {
+            double čas = 0.001 * časovač.Interval;
+            if (!přestřiženo)
+            {
+                //Změna úhlu při kruhovém pohybu
+                úhel += omega * čas;
+
+                //Výpočet středu kuličky
+                double úhelRad = úhel * Math.PI / 100;
+                double rozdílX = poloměrKružnice * Math.Cos(úhelRad);
+                double rozdílY = poloměrKružnice * Math.Sin(úhelRad);
+                xStředuKuličky = xStředuKružnice + rozdílX;
+                yStředuKuličky = yStředuKružnice + rozdílY;
+            }
+            else
+            {
+                //Přímočarý pohyb
+                xStředuKuličky += vx * čas;
+                yStředuKuličky += vy * čas;
+            }
+            Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics kp = e.Graphics;
+
+            //Souřadnice kuličky do intu
+
+            int xInt = Convert.ToInt32(xStředuKuličky);
+            int yInt = Convert.ToInt32(yStředuKuličky);
+
+            //Šňůra
+
+            if (!přestřiženo)
+                kp.DrawLine(Pens.CornflowerBlue, xStředuKružnice, yStředuKružnice, xInt, yInt);
+
+            //Kulička
+            int xLH = xInt - poloměrKuličky;
+            int yLH = yInt - poloměrKuličky;
+            int šířka = 2 * poloměrKuličky;
+            int výška = šířka;
+            kp.FillEllipse(Brushes.CornflowerBlue, xLH, yLH, šířka, výška);
+        }
+
+        private void tlPřestřihni_Click(object sender, EventArgs e)
+        {
+
+            přestřiženo = true;
+            tlPřestřihni.Enabled = false;
+            tlZnovu.Enabled = true;
+
+            //Výpočet rychlosti přímočarého pohybu
+            double omegaRadS = omega * Math.PI / 180;
+            double rychlost = omegaRadS * poloměrKružnice;
+            double směrRychlosti = úhel + 90;
+            double směrRad = směrRychlosti * Math.PI / 180;
+            vx = rychlost * Math.Cos(směrRad);
+            vy = rychlost * Math.Sin(směrRad);
+
+        }
+
+        private void tlZnovu_Click(object sender, EventArgs e)
+        {
+
+            přestřiženo = false;
+            úhel = 0;
+            tlPřestřihni.Enabled = true;
+            tlZnovu.Enabled = true;
+
+
+        }
+    }
+}
